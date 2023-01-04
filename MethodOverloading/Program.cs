ee7da144e212da21d5aa62c7bc329af91bed7ba5
ross dibartolo <dibartoloross@gmail.com>
2023-01-04 16:02:28 -0500

    inital commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,39 @@
 {
     public class Program
     {
+        public static int Add(int num1, int num2)
+            {
+            return num1 + num2;
+            }
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+        public static string Add(int num1, int num2, bool isTrue)
+        {
+            var sum = num1 + num2;
+
+            if (sum == 1)
+            {
+                return sum + " " + "dollar";
+            }
+            else if (sum > 1)
+            {
+                return sum + " " + "dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
+        
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Add(3, 5);
+            Add(4.53m, 3.10m);
+            Add(3, 6, true);
         }
     }
 }
